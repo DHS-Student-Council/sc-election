972ed5eb7feb586b1368b495f93936040b4c5301
sawzedong <saw.zedong@dhs.sg>
2022-03-19 16:10:48 +0800

    :bug: Fix relative (and index.html) pathnames issues

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -2,5 +2,10 @@ $( function() {
     $("#nav-placeholder").load("./templates/nav.html", function() {
         $("a[href*='" + location.pathname + location.search + "']").addClass("active");
         $("a[href*='" + location.pathname + location.search + "']").attr("aria-current", "page");
+
+        if(location.pathname === "/index.html") {
+            $("a[href*='/']").addClass("active");
+            $("a[href*='/']").attr("aria-current", "page");
+        }
     });
 });
