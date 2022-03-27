95aeb487e74e0b82841000a9a771bb2f54bdda3c
sawzedong <saw.zedong@dhs.sg>
2022-03-27 11:11:44 +0800

    :art: Rework navbar active system

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,13 +1,27 @@
 $( function() {
+    params = new URLSearchParams(window.location.search);
+    param_dict = {};
+    for (const param of params) {
+        param_dict[param[0]] = param[1];
+    }
+
+
     $("#nav-placeholder").load("./templates/nav.html", function() {
         path = location.pathname.replace("/sc-election", "")
 
-        $("a[href*='" + path + location.search + "']").addClass("active");
-        $("a[href*='" + path + location.search + "']").attr("aria-current", "page");
+        if(path === "/index.html" || path === "/") {
+            $("a#homepage").addClass("active");
+            $("a#homepage").attr("aria-current", "page");
+            console.log('ran index')
+        } else {
+            $("a[href*='" + path + location.search + "']").addClass("active");
+            $("a[href*='" + path + location.search + "']").attr("aria-current", "page");
+            console.log('ran other')
+        }
 
-        if(path === "/index.html") {
-            $("a[href*='/']").addClass("active");
-            $("a[href*='/']").attr("aria-current", "page");
+        if ("type" in param_dict) {
+            $("a[data-headertype*='" + param_dict["type"] + "']").addClass("active");
+            console.log('ran other')
         }
     });
 });
