ef7024ab7558346402a2e1a3195f3b42d30ee33b
sawzedong <saw.zedong@dhs.sg>
2022-03-27 11:20:50 +0800

    :truck: Move candidate data out of assets folder

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,7 +1,7 @@
 $( function() {
     $.ajax({
         type: "GET",
-        url: "./assets/datasheets/jh_candidates_experimental.csv",
+        url: "./candidate-data/datasheets/jh_candidates.csv",
         dataType: "text",
         success: function(data) {processData(data);}
      });
@@ -15,9 +15,9 @@ function processData(rawdata) {
         copy = $(`<div id=item${i+1} class="col-lg-4 col-md-6"></div>`);
         $('#item-placeholder').append(copy.append(contents));
 
-        $(`#item${i+1} .identifier`).text(data[i]["Candidate's ID"]);
-        $(`#item${i+1} .picture`).attr("src",`./assets/img/experimental/${data[i]["Catalog (Thumbnail)"]}`);
-        $(`#item${i+1} a`).attr("href", `./viewer.html?id=${data[i]["Candidate's ID"]}`);
-        $(`#item${i+1} .name`).text(data[i]["Candidate's Name"]);
+        $(`#item${i+1} .identifier`).text(data[i]["id"]);
+        $(`#item${i+1} .picture`).attr("src",`./candidate-data/experimental/${data[i]["catalog-thumbnail"]}`);
+        $(`#item${i+1} a`).attr("href", `./viewer.html?id=${data[i]["id"]}`);
+        $(`#item${i+1} .name`).text(data[i]["name"]);
     }
 }
