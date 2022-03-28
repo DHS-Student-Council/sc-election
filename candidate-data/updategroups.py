b5fce817ed8d988c2ac9101e0651e86af4607e7b
sawzedong <saw.zedong@dhs.sg>
2022-03-28 21:05:52 +0800

    :hammer: Add group csv file generator from xlsx file

---
+++ candidate-data/updategroups.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+
+def update(cat):
+    import pandas as pd
+    df = pd.read_excel("./candidate-data/datasheets/candidate_data_final.xlsx", sheet_name=(cat+"_candidate_data"), engine="openpyxl")
+    list_of_ids = list(df['id'])
+    list_of_names = list(df['name'])
+    list_of_groups = list(df['group_id'])
+
+    group_dict = {}
+
+    for i in range(len(list_of_ids)):
+        if list_of_groups[i] not in group_dict:
+            group_dict[list_of_groups[i]] = []
+            group_dict[list_of_groups[i]].append(list_of_ids[i] + " " + list_of_names[i])
+        else:
+            group_dict[list_of_groups[i]].append(list_of_ids[i] + " " + list_of_names[i])
+
+    for i in group_dict.keys():
+        group_dict[i] = [i, ', '.join(group_dict[i])]
+        for filename in os.listdir("./candidate-data/" +cat+ "_groupwork/"):
+            if i.upper() in filename.upper():
+                group_dict[i].append("./candidate-data/" +cat+ "_groupwork/"+filename)
+                
+    df_new = pd.DataFrame.from_dict(group_dict, orient="index", columns=["group_id", "members", "infographic"])
+    df_new.to_csv("./candidate-data/datasheets/group_"+cat+"_data.csv", index=False)
+
+    print(cat,"CSV update completed")
+
+    
+update("jh")
+update("sh")
