98b5cc83a013382d0eb29c0c95b10b53ff835491
sawzedong <saw.zedong@dhs.sg>
2022-03-30 21:01:23 +0800

    :hammer: Edit CSV updating file

--- candidate-data/updatecsv.py
+++ candidate-data/updatecsv.py
@@ -1,6 +1,27 @@
 import os
 import pandas as pd
 pd.options.mode.chained_assignment = None  # default='warn'
+"""
+# constants: please update prior to running
+# all data that requires manual entry (e.g. id, name, youtube links) must be in the excel
+# all others file paths will be auto generated
+
+# for each task, there should be a specific naming convention or format
+# add any keyword that is unique to that task's filepath to identify it
+# leave blank if the filepath should be left None (e.g. coming soon) / doesn't need auto updating (has manual data entry)
+# colname (key) : keyword (value)
+# all columns must be included in the keywords dictionary
+"""
+keywords = {
+    "id": None,
+    "name": None,
+    "catalog": "feature wall",
+    "profile": "profile",
+    "introvid-vid": "intro_video",
+    "introvid-thumbnail": "intro_thumbnail",
+    "dtalk-vid": None,
+    "dtalk-thumbnail": "##this value should never be accomplished##" # function update() sets any file which fails above values to dtalk-thumbnail
+}
 
 def update(cat):
     import pandas as pd
@@ -8,33 +29,23 @@ def update(cat):
     list_of_ids = list(df['id'])
     list_of_names = list(df['name'])
 
-    df["catalog"] = ""
-    df["profile"] = ""
-    df["introvid-vid"] = ""
-    df["introvid-thumbnail"] = ""
-    df["dtalk-vid"] = ""
-    df["dtalk-thumbnail"] = ""
+    for key in keywords.keys():
+        if key not in df.columns:
+            df[key] = ""
     
     for i in range(len(list_of_ids)):
         id = list_of_ids[i]
         name = list_of_names[i]
-        outcounter = 0
         for filename in os.listdir("./candidate-data/" + id + " " + name + "/"):
-            if "feature wall" in filename:
-                df['catalog'][i] = "./candidate-data/" + id + " " + name + "/" + filename
-            elif "profile" in filename:
-                df['profile'][i] = "./candidate-data/" + id + " " + name + "/" + filename
-            elif "intro_video" in filename:
-                df['introvid-vid'][i] = "./candidate-data/" + id + " " + name + "/" + filename
-            elif "intro_thumbnail" in filename:
-                df['introvid-thumbnail'][i] = "./candidate-data/" + id + " " + name + "/" + filename
-            else:
-                df['dtalk-thumbnail'][i] = "./candidate-data/" + id + " " + name + "/" + filename
-                outcounter += 1
-
-        if outcounter > 1:
-            print("DUPLICATE FILE TYPES DETECTED: ID", id)
-
+            success = 0
+            for key in keywords.keys():
+                if keywords[key] != None and keywords[key] in filename:
+                    df[key][i] = "./candidate-data/" + id + " " + name + "/" + filename
+                    success += 1
+            if success > 1:
+                print(f"Filepath matched more than 1 keyword: Matched {success} times. ID: {id}, filepath: {id} {name}/{filename}")
+            elif success == 0:
+                df["dtalk-thumbnail"][i] = "./candidate-data/" + id + " " + name + "/" + filename
 
     df.to_csv("./candidate-data/datasheets/"+cat+"_candidates.csv", index=False)
     print(cat, "CSV update completed")
